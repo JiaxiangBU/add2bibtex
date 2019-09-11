c14d54cb108f2aea71a534110374b20fbec50327
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-09-11 19:47:00 +0800

    add one more scenario for the function `get_wechat_author` and add the function is_wechat_author`.

--- R/add_wechat.R
+++ R/add_wechat.R
@@ -2,32 +2,32 @@
 
 
 add_wechat <- function(url = '') {
-    if (!stringr::str_detect(url, 'weixin')) {
-        stop("It is not a weixin url.")
-    }
-    text <- xml2::read_html(url)
+  if (!stringr::str_detect(url, 'weixin')) {
+    stop("It is not a weixin url.")
+  }
+  text <- xml2::read_html(url)
 
-    author <- get_wechat_author(text)
+  author <- get_wechat_author(text)
 
-    title <- text %>%
-        rvest::html_nodes('#activity-name') %>%
-        rvest::html_text() %>%
-        stringr::str_trim()
+  title <- text %>%
+    rvest::html_nodes('#activity-name') %>%
+    rvest::html_text() %>%
+    stringr::str_trim()
 
-    year <- Sys.Date() %>% str_sub(1, 4)
+  year <- Sys.Date() %>% str_sub(1, 4)
 
-    howpublished <- text %>%
-        rvest::html_nodes('#js_name') %>%
-        rvest::html_text() %>%
-        stringr::str_trim()
+  howpublished <- text %>%
+    rvest::html_nodes('#js_name') %>%
+    rvest::html_text() %>%
+    stringr::str_trim()
 
-    urldate <- Sys.Date()
+  urldate <- Sys.Date()
 
-    first_name <- stringr::str_replace_all(author, '[\\s，]', '_')
-    alias <- stringr::str_c(first_name, year)
+  first_name <- stringr::str_replace_all(author, '[\\s，]', '_')
+  alias <- stringr::str_c(first_name, year)
 
-    output <- glue::glue(
-        "@online{<<alias>>,
+  output <- glue::glue(
+    "@online{<<alias>>,
   author = {<<author>>},
   title = {<<title>>},
   year = <<year>>,
@@ -35,41 +35,48 @@ add_wechat <- function(url = '') {
   url = {<<url>>},
   urldate = {<<urldate>>}
 }"
-        ,
-        .open = "<<"
-        ,
-        .close = ">>"
-    )
+    ,
+    .open = "<<"
+    ,
+    .close = ">>"
+  )
 
-    clip_and_print(output)
+  clip_and_print(output)
 }
 
 #' @importFrom rvest html_nodes html_text
 #' @importFrom stringr str_trim
 get_wechat_raw_text <- function(html, pattern) {
-    html %>%
-        rvest::html_nodes(pattern) %>%
-        rvest::html_text() %>%
-        max %>%
-        stringr::str_trim()
+  html %>%
+    rvest::html_nodes(pattern) %>%
+    rvest::html_text() %>%
+    max %>%
+    stringr::str_trim()
 }
 
 #' @importFrom stringr str_detect str_trim str_flatten str_extract str_remove_all str_trim str_replace_all
 get_wechat_author <- function(text) {
-    if (!text %>%
-        get_wechat_raw_text('#js_author_name') %>%
-        is.na()) {
-        get_wechat_raw_text(text, '#js_author_name') %>%
-            .[!stringr::str_detect(., '：')] %>%
-            stringr::str_trim() %>%
-            stringr::str_flatten('') %>%
-            stringr::str_extract("[\\p{Han}A-z\\s|:：——，]+")
-    } else if (!text %>%
-               get_wechat_raw_text('#js_author') %>%
-               is.na()) {
-        get_wechat_raw_text(text, '#js_author') %>%
-            stringr::str_remove_all("作者|等") %>%
-            stringr::str_trim() %>%
-            stringr::str_replace_all("\\s", " and ")
-    }
+  if (is_wechat_author(text, '#js_author_name')) {
+    get_wechat_raw_text(text, '#js_author_name') %>%
+      .[!stringr::str_detect(., '：')] %>%
+      stringr::str_trim() %>%
+      stringr::str_flatten('') %>%
+      stringr::str_extract("[\\p{Han}A-z\\s|:：——，]+")
+  } else if (is_wechat_author(text, '#js_author')) {
+    get_wechat_raw_text(text, '#js_author') %>%
+      stringr::str_remove_all("作者|等") %>%
+      stringr::str_trim() %>%
+      stringr::str_replace_all("\\s", " and ")
+  } else if (is_wechat_author(text, ".rich_media_meta_text")) {
+    get_wechat_raw_text(text, ".rich_media_meta_text") %>%
+      stringr::str_remove_all("作者|等") %>%
+      stringr::str_trim() %>%
+      stringr::str_replace_all("\\s", " and ")
+  }
+}
+
+is_wechat_author <- function(text, pattern) {
+  !text %>%
+    get_wechat_raw_text(pattern) %>%
+    is.na()
 }
